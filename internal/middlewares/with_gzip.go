4d2f406cbdb4c8acf74cceec6058d0b8859a732b
Dmitry Scheklein <Dmitry.Scheklein@x5.ru>
2026-04-02 21:55:51 +0500

    feat: iter8 add_gzip_middleware

--- internal/middlewares/with_gzip.go
+++ internal/middlewares/with_gzip.go
@@ -19,33 +19,44 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 func WithGzip(h http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+				validTypes := map[string]bool{
+					"application/json": true,
+					"text/html":        true,
+				}
+
+				contentType := r.Header.Get("Content-Type")
+
+				_, ok := validTypes[contentType]
+				if !ok {
+					h.ServeHTTP(w, r)
+					return
+				}
+
+				gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+				if err != nil {
+					io.WriteString(w, "error while create gzip")
+					return
+				}
+				defer gz.Close()
+
+				w.Header().Set("Content-Encoding", "gzip")
+				h.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+
+				return
+			}
+
+			if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+				reader, err := gzip.NewReader(r.Body)
+				if err != nil {
+					io.WriteString(w, "error while read gzip")
+					return
+				}
+				r.Body = reader
 				h.ServeHTTP(w, r)
 				return
 			}
-
-			validTypes := map[string]bool{
-				"application/json": true,
-				"text/html":        true,
-			}
-
-			contentType := r.Header.Get("Content-Type")
-
-			_, ok := validTypes[contentType]
-			if !ok {
-				h.ServeHTTP(w, r)
-				return
-			}
-
-			gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
-			if err != nil {
-				io.WriteString(w, err.Error())
-				return
-			}
-			defer gz.Close()
-
-			w.Header().Set("Content-Encoding", "gzip")
-			h.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+			h.ServeHTTP(w, r)
 		},
 	)
 }
